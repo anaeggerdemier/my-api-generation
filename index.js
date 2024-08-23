40888ea5be4ae3175aa641fc96002602f928e02c
anaeggerdemier <anaeggerdemier@outlook.com>
2024-08-23 17:44:42 -0300

    Adicionando rota para exibir mensagem 'API is running'

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
